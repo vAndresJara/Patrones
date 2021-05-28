a80458e5facf648eed4aad855db2edb45f09de32
vAndresJara <victor.jarae@gmail.com>
2021-05-27 23:26:13 -0400

    Revisión

---
+++ src/bancothink_fabrica/JefeSucursal.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bancothink_fabrica;
+
+/**
+ *
+ * @author Cristian Matus
+ */
+public class JefeSucursal {
+    
+    private int codigoJefe;
+    
+    public JefeSucursal(int codigoJefe) {
+        this.codigoJefe = codigoJefe;
+    }
+
+    public int getCodigoJefe() {
+        return codigoJefe;
+    }
+
+    public void setCodigoJefe(int codigoJefe) {
+        this.codigoJefe = codigoJefe;
+    }
+}
